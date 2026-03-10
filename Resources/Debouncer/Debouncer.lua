0d5687f3a00b314d1ec419dce6ca916e8e2701c9
JakeyRoundHead <112113737+J4KEWasNotHere@users.noreply.github.com>
2026-03-10 21:04:14 +0200

    Refactor Debouncer methods to use class syntax

--- Resources/Debouncer/Debouncer.lua
+++ Resources/Debouncer/Debouncer.lua
@@ -1,79 +1,53 @@
+--!strict
 --!optimize 2
 
---[[
-	|| Debouncer
-	
-	|| Used for delaying certain events and functions, for a certin amount of time.
-	|| By: @jakeboygamer64
-	
-	|| Usage:
-	||	local Debouncer = require(path.to.Debouncer)
-	||	local db = Debouncer.new()
-	||
-	||	game:GetService("RunService").Heartbeat:Connect(function()
-	||		db:Run("MyFunction", 2, function()
-	||			print("Hello World!")
-	||		end)
-	||	end)
-	||
-	||	task.delay(5, function() 
-	||		db:Skip("MyFunction") -- Cancels the debounce for "MyFunction"
-	||	end)
-	||
-	||	db:Destroy() -- Destroys the debounce object and clears all active timers
-]]
+local Types = require("@self/Types")
 
 local Debounce = {}
 Debounce.__index = Debounce
 
+local Types = require(script.Types)
+
 function Debounce.new()
-	local self = setmetatable({}, Debounce)
-	self._states = {} -- Tracks active debounce timers
-	return self
+	local self = {_states = {}}
+	return setmetatable(self, Debounce)
 end
 
 -- Runs a function if the key isn't in an active debounce
-function Debounce:Run(key, delayTime, fn)
+function Debounce.Run(self: Types.Class, key: string, delayTime: number, fn: () -> ())
 	if self:IsActive(key) then
 		return false
 	end
-
-	local state = {}
-	self._states[key] = state
-
-	state.timer = task.delay(delayTime, function()
-		if self._states[key] == state then
+	
+	self._states[key] = {
+		timer = task.delay(delayTime, function()
 			self._states[key] = nil
-		end
-	end)
+		end)
+	}
 
 	fn()
 	return true
 end
 
 -- Checks if a debounce is active for the given key
-function Debounce:IsActive(key)
+function Debounce.IsActive(self: Types.Class, key: string)
 	return self._states[key] ~= nil
 end
 
 -- Cancels a running debounce
-function Debounce:Skip(key)
+function Debounce.Skip(self: Types.Class, key: string)
 	local state = self._states[key]
-	if state and state.timer then
-		pcall(task.cancel, state.timer)
-	end
+	if state and state.timer then pcall(task.cancel, state.timer) end
 	self._states[key] = nil
 end
 
--- Cancels all debounces and clears the table
-function Debounce:Destroy()
+-- Cancels all debounces and clears the table.
+function Debounce.Clean(self: Types.Class)
 	for key, state in pairs(self._states) do
-		if state.timer then
-			pcall(task.cancel, state.timer)
-		end
+		if state.timer then pcall(task.cancel, state.timer) end
 		self._states[key] = nil
 	end
 	table.clear(self._states)
 end
 
-return Debounce.new()
+return Debounce
